57df7ee6f4608e686aac433c88f4545c676404d7
lijinyang <kylin27@126.com>
2020-02-10 11:38:51 +0800

    增加生产模式

--- webpack.config.js
+++ webpack.config.js
@@ -86,3 +86,15 @@ module.exports = {
     },
     devtool: '#eval-source-map'
 };
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.devtool = '#source-map';
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+
+    ])
+}
